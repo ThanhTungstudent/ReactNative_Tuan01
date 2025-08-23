371db245c1fa15924314214a88cedd49c138a398
H82M19\Student <Student>
2025-08-23 14:01:58 +0700

    Bai 3

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const bai1_1 = require("./bai1");
 const bai2_1 = require("./bai2");
+const bai3_1 = require("./bai3");
 // Bai 1
 console.log("Bai 1");
 const person = new bai1_1.Person("Pham Thanh Tung", 20);
 person.information();
 // Bai 2
-console.log("\n Bai 2");
+console.log("\nBai 2");
 const student = new bai2_1.Student("Pham Thanh Tung", 20, 10);
 student.allInfo();
+// Bai 3
+console.log("\nBai 3");
+const car = new bai3_1.Car("Toyota", "Camry", 2025);
+car.showCarInfo();
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Person } from "./bai1";
 import { Student } from "./bai2";
+import { Car } from "./bai3";
 
 
 // Bai 1
@@ -8,6 +9,12 @@ const person = new Person("Pham Thanh Tung", 20)
 person.information();
 
 // Bai 2
-console.log("\n Bai 2");
+console.log("\nBai 2");
 const student = new Student("Pham Thanh Tung", 20, 10);
 student.allInfo()
+
+// Bai 3
+console.log("\nBai 3");
+const car = new Car("Toyota", "Camry", 2025);
+car.showCarInfo()
+
