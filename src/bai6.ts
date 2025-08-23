30f9a6b56d65cdec6751efb48cd4ca8b3eb32217
H82M19\Student <Student>
2025-08-23 14:22:14 +0700

    Bai 6

---
+++ src/bai6.ts
@@ -0,0 +1,10 @@
+class Book {
+    title: string
+    author: string
+    year: number
+    constructor(title: string, author: string, year: number){
+        this.title = title
+        this.author = author
+        this.year = year
+    }
+}
